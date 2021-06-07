382547b643ad70c968caf3d0a95e084faf009934
Yaritza-Aleman <Yrtz.aleman@yahoo.com>
2021-06-07 11:38:56 -0500

    Complete APOD

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+var apod = {
+
+    //Create a random date
+randomDate: function(start, end) {
+    //Randomize the date https://gist.github.com/miguelmota/5b67e03845d840c949c4
+    let date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  
+    //Format the date
+    let d = date.getDate();
+    let m = date.getMonth() + 1; //In JS months start at 0
+    let y = date.getFullYear();
+  
+    //Change the month and day strings so that they match the documented format.
+    if(m < 10){
+      m = '0'+m
+    }
+  
+    if(d < 10){
+      d = '0'+d
+    }
+  
+    return `${y}-${m}-${d}`;
+  },
+  
+    // Application Constructor
+    init: function() {
+        let date = this.randomDate(new Date(1995, 5, 16), new Date());
+        var url = "https://api.nasa.gov/planetary/apod?api_key=e1neM7gYxGE9hJ5BcZlrt9epGux7iUFKWSaYOdKq&date=" + date;
+        
+
+        $.ajax({
+            url: url
+        }).done(function(result){
+            $("#apodTitle").text(result.title);
+           //If the media type is video hide the image elements and display a video.
+if(result.media_type === 'video') {
+    $("#apodImg").hide();
+    $("#apodVideo > iframe").attr("src", result.url).show();
+  }else{
+    $("#apodVideo").hide();
+    $("#apodImg").attr("src", result.url).attr('alt', result.title).show();
+  }
+            $("#apodCopyright").text("Copyright: " + result.copyright);
+            $("#apodDate").text("Date: " + date);
+            $("#apodDesc").text(result.explanation);
+          }).fail(function(result){
+          console.log(result);
+        });
+    },
+};
+
+apod.init();
